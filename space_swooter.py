bd949a47a05582a1cad6158e0663cf7cdbf1ce4d
Michal Kowalczuk <michkowalczuk@gmail.com>
2017-03-08 19:09:06 +0100

    Initial commit

--- space_swooter.py
+++ space_swooter.py
@@ -1,4 +1,6 @@
 # Pygame template - skeleton for a new pygame project
+from lib2to3.refactor import _EveryNode
+
 import pygame
 from pygame import Color
 import random
@@ -35,7 +37,48 @@ class Player(pygame.sprite.Sprite):
         if self.rect.left < 0:
             self.rect.left = 0
 
-class Mob()
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30,30))
+        self.image.fill(Color('red'))
+        self.rect = self.image.get_rect()
+
+        self.gen_new_mob()
+
+    def update(self):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+
+        if self.rect.top > HEIGHT or self.rect.right < 0 or self.rect.left > WIDTH:
+            self.gen_new_mob()
+
+    def gen_new_mob(self):
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-150, -100)
+        self.speed_x = random.randrange(-3, 3)
+        self.speed_y = random.randrange(1, 8)
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10,20))
+        self.image.fill(Color('yellow'))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speed_y = -10
+
+    def update(self):
+        self.rect.y += self.speed_y
+        if self.rect.bottom < 0:
+            self.kill()
+
 
 # INITIALIZE PYGAME AND CREATE WINDOW
 pygame.init()
@@ -48,6 +91,13 @@ all_sprites = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
 
+mobs = pygame.sprite.Group()
+for i in range(8):
+    mob = Mob()
+    mobs.add(mob)
+    all_sprites.add(mob)
+
+bullets = pygame.sprite.Group()
 
 # GAME LOOP
 running = True
@@ -60,6 +110,10 @@ while running:
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+
 
     # UPDATE
     all_sprites.update()
